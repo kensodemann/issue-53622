fa41fe29d6e7d246008d283b08805bf1dfa15e29
Ken Sodemann <kenneth.sodemann@outsystems.com>
2025-05-14 10:27:22 -0500

    use window name system

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,6 +12,6 @@ export class HomePage {
   constructor() {}
 
   openBrowser() {
-    Browser.open({ url: 'https://www.npr.org' });
+    Browser.open({ url: 'https://www.npr.org', windowName: '_system' });
   }
 }
